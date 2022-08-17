02bf973256b52c8697028ac814c1be1466f37ca9
César Aparicio <clark012389@hotmail.com>
2022-08-17 02:56:34 -0500

    Complementando ejercicio

--- fetchAPI/example_fetch.js
+++ fetchAPI/example_fetch.js
@@ -1,3 +1,6 @@
+/** ESTILO 1 con arrow functions */
+
+/*
 const url = " https://pokeapi.co/api/v2/pokemon/ditto";
 
 const getPokemon = async () => {
@@ -13,3 +16,22 @@ const getPokemon = async () => {
 const pokemon = getPokemon().then((pokemon) => console.log(pokemon));
 
 console.log(pokemon);
+*/
+
+/** ESTILO 2 con funciones normales*/
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+
+async function getPokemon() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(`Ups..! hubo un error: ${error.message}`);
+  }
+}
+
+getPokemon()
+  .then((pokemon) => console.log(pokemon))
+  .catch((e) => console.log(e))
+  .finally(() => console.log("Peticion terminada"));
