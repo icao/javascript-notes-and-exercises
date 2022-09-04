f53761a95e16c02d1d146557bba3e15017908170
César Aparicio <clark012389@hotmail.com>
2022-09-04 14:23:17 -0500

    Agregando ejemplos de añadir elementos al inicio y final,  sin mutar y mutando el arreglo original

--- _EJERCICIOS/arrays/_ejercicios/anadir-eliminar-elementos-arrays/ejercicio01.js
+++ _EJERCICIOS/arrays/_ejercicios/anadir-eliminar-elementos-arrays/ejercicio01.js
@@ -1,7 +1,23 @@
-const animales = ['leon', 'lobo', 'tigre', 'perro']
+/**
+ * Añadiendo elementos a un arreglo.
+ */
 
+const animales = ["leon", "lobo", "tigre", "perro", "ballena", "rinoceronte"];
+const nuevoAnimal = "AGUILA";
 
+// Añadir un elemento al inicio - MUTANDO
+// animales.unshift(nuevoAnimal);
+// console.log(animales); // ["AGUILA", "leon", "lobo", "tigre", "perro", "ballena", "rinoceronte"]
 
-animales.unshift('dragon')
+// Añadir un elemento al inicio - SIN MUTAR
+// const nuevosAnimales = [nuevoAnimal, ...animales];
+// console.log(nuevosAnimales); // ["AGUILA", "leon", "lobo", "tigre", "perro", "ballena", "rinoceronte"]
 
-console.log(arregloStrings)
+// Añadir un elemento al final - MUTANDO
+animales.push(nuevoAnimal);
+console.log(animales); // ["leon", "lobo", "tigre", "perro", "ballena", "rinoceronte", "AGUILA"]
+
+// Añadir un elemento al final - SIN MUTAR
+const nuevosAnimales = [...animales, nuevoAnimal]
+console.log(nuevosAnimales); // ["leon", "lobo", "tigre", "perro", "ballena", "rinoceronte", "AGUILA"]
+
