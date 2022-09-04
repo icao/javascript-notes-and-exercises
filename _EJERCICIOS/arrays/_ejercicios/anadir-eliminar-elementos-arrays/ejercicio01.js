c7837b74273b0e7cec3bc4bcca8e622366650ddd
César Aparicio <clark012389@hotmail.com>
2022-09-04 15:38:28 -0500

    Agregando ultimos ejemplos, insertando un elemento en una posicion N, mutando y sin mutar el arreglo original

--- _EJERCICIOS/arrays/_ejercicios/anadir-eliminar-elementos-arrays/ejercicio01.js
+++ _EJERCICIOS/arrays/_ejercicios/anadir-eliminar-elementos-arrays/ejercicio01.js
@@ -14,10 +14,26 @@ const nuevoAnimal = "AGUILA";
 // console.log(nuevosAnimales); // ["AGUILA", "leon", "lobo", "tigre", "perro", "ballena", "rinoceronte"]
 
 // Añadir un elemento al final - MUTANDO
-animales.push(nuevoAnimal);
-console.log(animales); // ["leon", "lobo", "tigre", "perro", "ballena", "rinoceronte", "AGUILA"]
+// animales.push(nuevoAnimal);
+// console.log(animales); // ["leon", "lobo", "tigre", "perro", "ballena", "rinoceronte", "AGUILA"]
 
 // Añadir un elemento al final - SIN MUTAR
-const nuevosAnimales = [...animales, nuevoAnimal]
-console.log(nuevosAnimales); // ["leon", "lobo", "tigre", "perro", "ballena", "rinoceronte", "AGUILA"]
+// const nuevosAnimales = [...animales, nuevoAnimal]
+// console.log(nuevosAnimales); // ["leon", "lobo", "tigre", "perro", "ballena", "rinoceronte", "AGUILA"]
 
+// Añadir un elemento en el indice N - MUTANDO
+const index = 2;
+// animales.splice(index, 0, nuevoAnimal);
+// console.log(animales); // ["leon", "lobo", "AGUILA","tigre", "perro", "ballena", "rinoceronte"]
+// console.log(animales[index]); // "AGUILA"
+
+// Añadir un elemento en el indice N - SIN MUTAR
+const nuevosAnimales = [
+  ...animales.slice(0, 2), // ["leon", "lobo"]
+  nuevoAnimal, // ["AGUILA"]
+  ...animales.slice(2), // ["tigre", "perro", "ballena", "rinoceronte"]
+];
+
+
+console.log(nuevosAnimales) // ["leon", "lobo", "AGUILA","tigre", "perro", "ballena", "rinoceronte"]
+console.log(nuevosAnimales[index]) // "AGUILA"
