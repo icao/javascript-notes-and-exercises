b6748a6961a61688593622871534ecba9595f575
César Aparicio <clark012389@hotmail.com>
2022-09-06 18:23:34 -0500

    Agregando ejemplos sobre eliminar e; primer elemento de un array

---
+++ _EJERCICIOS/arrays/_ejercicios/anadir-eliminar-elementos-arrays/ejercicio02.js
@@ -0,0 +1,15 @@
+/**
+ * eliminando elementos a un arreglo.
+ */
+
+const animales = ["leon", "lobo", "tigre", "perro", "ballena", "rinoceronte"];
+
+// Eliminar un elemento al inicio - MUTANDO
+// animales.shift();
+// console.log(animales); // ["lobo", "tigre", "perro", "ballena", "rinoceronte"];
+
+// Eliminar un elemento al inicio - SIN MUTAR
+// const nuevosAnimales = [...animales.slice(1)];
+// const nuevosAnimales = [...animales.slice(0)];
+// console.log(nuevosAnimales); // ["lobo", "tigre", "perro", "ballena", "rinoceronte"];
+
