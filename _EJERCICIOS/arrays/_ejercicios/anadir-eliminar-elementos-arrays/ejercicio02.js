e7c3a437b479b0f80706ce62e3ac9417ea2e3ff5
César Aparicio <clark012389@hotmail.com>
2022-09-07 14:41:25 -0500

    Agregado , ejemplos eliminando el ultimo elemento de un array

--- _EJERCICIOS/arrays/_ejercicios/anadir-eliminar-elementos-arrays/ejercicio02.js
+++ _EJERCICIOS/arrays/_ejercicios/anadir-eliminar-elementos-arrays/ejercicio02.js
@@ -13,3 +13,15 @@ const animales = ["leon", "lobo", "tigre", "perro", "ballena", "rinoceronte"];
 // const nuevosAnimales = [...animales.slice(0)];
 // console.log(nuevosAnimales); // ["lobo", "tigre", "perro", "ballena", "rinoceronte"];
 
+// Eliminar un elemento al final - MUTANDO
+// Usando pop()
+// animales.pop();
+// console.log(animales); // ["leon", "lobo", "tigre", "perro", "ballena"];
+
+// Usando splice()
+// animales.splice(animales.length - 1, 1);
+// console.log(animales); // ["leon", "lobo", "tigre", "perro", "ballena"];
+
+// Eliminar un elemento al final - SIN MUTAR
+// const nuevosAnimales = [...animales.slice(0, animales.length - 1)];
+// console.log(nuevosAnimales);
