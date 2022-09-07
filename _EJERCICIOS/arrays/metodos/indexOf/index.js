5799da2554cfcd8d58a6bb2834e82b0359c1242b
César Aparicio <clark012389@hotmail.com>
2022-09-07 16:12:00 -0500

    Agregado teoria sobre indexOf

--- _EJERCICIOS/arrays/metodos/indexOf/index.js
+++ _EJERCICIOS/arrays/metodos/indexOf/index.js
@@ -1,3 +1,28 @@
+/**
+ * ------------------------------
+ * indexOf()
+ * ------------------------------
+ *
+ * El método indexOf() retorna la posición de la primera aparicion de un valor buscado en un array.
+ *  - Si el valor es encontrado, regresa el index de la posición del elemento en el array.
+ *  - Si el valor NO es encontrado, regresa -1, como indicador de que no se encontro el elemento dentro del array.
+ *
+ * indexOf() compara los elementos haciendo una igualdad estricta. Funciona con valores primitivos.
+ *
+ * Sintaxis: arrray.indexOf(seachElement, fromIndex)
+ * Donde:
+ *  - searchElement: El valor del elemento a buscar.
+ *  - fromIndex: El valor de indice desde donde comenzará la busqueda.
+ *               Por default es 0, por lo tanto es un parámetro opcional.
+ *               Si el valor es mayor que la longitud del array, devolverá -1,
+ *               ya que no se encuentra dentro de la longitud del array.
+ *               Si el valor es negativo, comenzará apartir del ultimo elemento
+ *               restando las posiciones.
+ *
+ * Retorno: El primer indice del elemento encontrado.
+ *          -1 Si no es encontrado
+ * */
+
 const arrayNumerico = [1, 2, 3, 4, 5];
 const index = arrayNumerico.indexOf(8);
 console.log(index); //-1
