e7289b521b536b80c4775e5d445e342b9a09e4ac
César Aparicio <clark012389@hotmail.com>
2022-09-07 15:01:37 -0500

    Agregado ejemplo , eliminar un elementro de cualquier indice

--- _EJERCICIOS/arrays/_ejercicios/anadir-eliminar-elementos-arrays/ejercicio02.js
+++ _EJERCICIOS/arrays/_ejercicios/anadir-eliminar-elementos-arrays/ejercicio02.js
@@ -25,3 +25,18 @@ const animales = ["leon", "lobo", "tigre", "perro", "ballena", "rinoceronte"];
 // Eliminar un elemento al final - SIN MUTAR
 // const nuevosAnimales = [...animales.slice(0, animales.length - 1)];
 // console.log(nuevosAnimales);
+
+// Eliminar cualquier elemento de un array - MUTANDO
+let indexToDelete = 2;
+// console.log(animales) //  ["leon", "lobo", "tigre", "perro", "ballena", "rinoceronte"]
+// animales.splice(indexToDelete, 1); // Elimina index 2 : 'TIGRE'
+// console.log(animales); // [ 'leon', 'lobo', 'perro', 'ballena', 'rinoceronte' ]
+
+// Eliminar cualquier elemento de un array - SIN MUTAR
+
+const nuevosAnimales = [
+  ...animales.slice(0, indexToDelete),
+  ...animales.slice(indexToDelete + 1),
+];
+
+console.log(nuevosAnimales); // [ 'leon', 'lobo', 'perro', 'ballena', 'rinoceronte' ]
