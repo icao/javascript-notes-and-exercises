6dc878cc42902b712c22ba6668aae6d94ea775bb
César Aparicio <clark012389@hotmail.com>
2022-09-19 18:57:13 -0500

    Agregando carpeta para la seccion de copias

---
+++ copia-superficial-vs-profunda/index.js
@@ -0,0 +1,6 @@
+/** 
+ * seccion para
+ * Shallow copy (copia superficial) y deep copy (copia profunda)
+*/
+// investigar structuredClone()   metodo nuevo para JS pero aun no soportado pro todos lo navegadores , o buscar un pollyfil
+// ver midudev : https://www.twitch.tv/videos/1595552361
